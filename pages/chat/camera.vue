3bc443b7316e6696d80771c9f02519f9f7150068
Sosthene Hounsokou <sosthenehounsokou@gmail.com>
2024-05-15 16:25:10 +0100

    Camera page is set

--- pages/chat/camera.vue
+++ pages/chat/camera.vue
@@ -1,5 +1,5 @@
 <template>
-        <div  class="w-screen h-screen bg-cover bg-fixed bg-no-repeat bg-center">
+    <div class="w-screen h-screen bg-cover bg-fixed bg-no-repeat bg-center">
         <div class="h-screen py-16 flex flex-col justify-around items-center bg-black bg-opacity-65">
             <div class="max-w-md flex flex-col justify-center items-center">
                 <img src="~/assets/images/bot.webp" alt="" class="rounded-full w-24 h-24 mb-5">
@@ -7,15 +7,34 @@
             </div>
             <div class="max-w-md w-full flex justify-center">
                 <div class="w-32 h-32 rounded-3xl flex justify-center items-center bg-white">
-                    <IconsCamera :height="'3'" :width="'3'" :color="'#0B873A'"/>
+                    <IconsCamera @click="uploadImage" :height="'3'" :width="'3'" :color="'#0B873A'"/>
                 </div>
             </div>
             <div class="max-w-md w-full flex justify-center px-5 md:px-10">
-                <div id="end_chat" class="w-full h-16 rounded-md bg-red-600 flex justify-center items-center">
-                    <NuxtLink to="/chat"><IconsPhoneEnd :height="'3'" :width="'3'" :color="'#fff'"/></NuxtLink>
+                <div id="end_chat" class="w-full h-16 rounded-full bg-red-600 flex justify-center items-center">
+                    <NuxtLink to="/chat"><IconsCircleXmark :height="'3'" :width="'3'" :color="'#fff'"/></NuxtLink>
                 </div>
             </div>
         </div>
+
+        <input type="file" ref="fileInput" class="hidden" @change="handleFileUpload">
     </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const fileInput = ref<HTMLInputElement | null>(null);
+
+const uploadImage = () => {
+    if (fileInput.value) {
+        fileInput.value.click();
+    }
+};
+
+const handleFileUpload = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    const file = target.files ? target.files[0] : null;
+    if (file) {
+        alert('Fichier sélectionné');
+        console.log('Fichier sélectionné:', file);
+    }
+};
+</script>
